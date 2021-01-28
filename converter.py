baba1b483d19dd073e18e64d4caa41de033af864
kkkaren <zhoyizhen_1998@hotmail.com>
2021-01-28 19:51:47 +0800

    save path destination

--- converter.py
+++ converter.py
@@ -4,4 +4,5 @@ path = path.rstrip()
 xls = pandas.ExcelFile(path)
 for sheet_name in xls.sheet_names:
     datafile = pandas.read_excel(path, sheet_name= sheet_name)
-    datafile.to_csv(r'/Users/zhouyizhen/coursemap/data/csv_files/new_'+sheet_name+'.csv') 
+    savepath = os.path.join(os.path.dirname(path),"csv_files","new_"+sheet_name+".csv")
+    datafile.to_csv(savepath) 

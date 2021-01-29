b8a74ec1406d407e2d95c5147a44c623eceab815
Patrick Walls <pwalls@math.ubc.ca>
2021-01-29 15:05:56 -0800

    Edit excel_to_csv.py script.
    Rename the script from converter.py to excel_to_csv.py. Remove unnecessary imports. Rename destination folder from csv_files/ to csv/. Remove new_ prefix from csv file names. Create csv/ folder if does not exist.

--- data/converter.py
+++
@@ -1,9 +0,0 @@
-import sys,re,os,pandas,fileinput
-script_path = os.path.abspath(__file__)
-folder_path = os.path.dirname(script_path)
-path = os.path.join(folder_path,"data.xlsx")
-xls = pandas.ExcelFile(path)
-for sheet_name in xls.sheet_names:
-    datafile = pandas.read_excel(path, sheet_name= sheet_name)
-    savepath = os.path.join(folder_path,"csv_files","new_"+sheet_name+".csv")
-    datafile.to_csv(savepath) 

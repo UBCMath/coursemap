914420f9460f0c97c017724ddc1281c8b258b0e4
kkkaren <zhoyizhen_1998@hotmail.com>
2021-01-30 06:44:41 +0800

    change the input path

--- converter.py
+++
@@ -1,8 +0,0 @@
-import sys,re,os,pandas,fileinput
-path = input("input file path here : ")
-path = path.rstrip()
-xls = pandas.ExcelFile(path)
-for sheet_name in xls.sheet_names:
-    datafile = pandas.read_excel(path, sheet_name= sheet_name)
-    savepath = os.path.join(os.path.dirname(path),"csv_files","new_"+sheet_name+".csv")
-    datafile.to_csv(savepath) 

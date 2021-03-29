ae8125d09abbd905a44d497d0f4df1552340ca4d
Wuyang Li <40313248+LeoLee5566@users.noreply.github.com>
2021-03-30 04:00:05 +0800

    change click functionality

--- js/click.js
+++ js/click.js
@@ -1,10 +1,26 @@
 // click a course to mark it
-function clickcircle(d) {
+function clickcircle(event, d) {
+    d.attr("ini_x", d.select(".course").attr("cx")); //set initial position for dragging
+    d.attr("ini_y", d.select(".course").attr("cy"));
+
+    if (event.type == "click") { // drag nodes should not change node's status
+        switch (parseInt(d.attr("status"))) {
+            case 0:
+                d.attr("status", 1);
+                break;
+            case 1:
+                d.attr("status", 2);
+                return;
+            case 2:
+                d.attr("status", 0);
+                break;
+        }
+    }
+
+    console.log(d.attr("status"));
     const course_num = d.attr("id");
 
     var c_status;
-    d.attr("ini_x", d.select(".course").attr("cx")); //set initial position for dragging
-    d.attr("ini_y", d.select(".course").attr("cy"));
     d.select(".course")
         .each(function(d) {
             c_status = d3.select(this).classed("course-click");
